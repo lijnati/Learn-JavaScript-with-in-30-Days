3c0ba73d3dfc8d544a6e02caea2709ff36f0cb04
ceoyegetaneh <lijnati3@gmail.com>
2023-05-16 19:24:42 +0400

    Day #3 Operators and Expressions

---
+++ Day-3/main.js
@@ -0,0 +1,23 @@
+//Arithmetic Operators
+
+var a = 10;
+var b = 5;
+var addition = a + b; console.log(addition)// 15
+var multiplication = a * b; console.log(multiplication)// 50
+
+//Comparison Operators
+var x = 5;
+var y = 10;
+var isEqual = x == y; console.log(isEqual)// false
+var isNotEqual = x != y; console.log(isNotEqual)// true
+
+//Logical Operators
+var isTrue = true;
+var isFalse = false;
+var result1 = isTrue && isFalse; console.log(result1)// false
+var result2 = isTrue || isFalse; console.log(result2)// true
+
+//Assignment Operators
+var num = 10;
+num += 5; 
+console.log(num)// num becomes 15
